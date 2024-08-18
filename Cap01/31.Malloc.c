e673bafbcb28f770002aa49bbab6cef708571347
mgcruzp <150625723+mgcruzp@users.noreply.github.com>
2024-08-18 17:41:02 -0500

    Update and rename malloc.c to 31.Malloc.c

---
+++ Cap01/31.Malloc.c
@@ -0,0 +1,29 @@
+/***********************************************************************
+ * Autor: G. Cruz
+ * Materia: Sistemas Operativos
+ * Fecha: 1-08-24
+ ***********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv){
+  
+  int *varptr1=malloc(4*sizeof(int));
+  int *varptr2=malloc(sizeof(int[4]));
+  int *varptr3=malloc(4*sizeof*varptr3);
+  
+  if(varptr1){
+    for(int i=0;i<4;i++)
+      varptr1[i]=i*i;
+    for(int i=0;i<4;i++)
+      printf("varptr1[%d]\t == %d\n",i,varptr1[i]);
+  }
+    
+  printf("Se libera memoria\n");
+  free(varptr1);
+  free(varptr2);
+  free(varptr3); 
+  
+  return 0;
+}
